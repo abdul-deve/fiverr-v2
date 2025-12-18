fc5e1894a0536a27c4ff84b60afb0f2d58f3fc7f
Abdul Mateen <abdulmateenaawan@gmail.com>
2025-12-18 16:30:22 +0500

    make changes in otp_manager model

--- otp/models.py
+++ otp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from lib.models import TimeStamp
 
-# Create your models here.
+User = get_user_model()
+class OTPManager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="otp")
+    secret_key = models.CharField(max_length=32, null=True, blank=True)
+    attempts = models.IntegerField(default=0)
+    last_attempt = models.DateTimeField(null=True, blank=True)
+    issued_at = models.DateTimeField(null=True, blank=True)
+    resend_attempts = models.IntegerField(default=0)
+    blocked_until = models.DateTimeField(null=True, blank=True)
+
+
