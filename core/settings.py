59c85300b2d0acb1ac315f07f0d7d725c4ef01a4
Abdul Mateen <abdulmateenaawan@gmail.com>
2025-12-18 16:24:04 +0500

    setup settings for apps

--- core/settings.py
+++ core/settings.py
@@ -18,6 +18,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -27,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'otp',
+    'lib',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +114,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.User'
